1cb056d66316ffa1e2767aa2193e2dc606b7e316
Daniel EPR94 <danielperezrossi@gmail.com>
2022-04-19 19:41:16 -0300

    add Event Listeners

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ function operate(a,b){
     let result = add(a,b);
     return "El resultado es " + result;
 }
+////////////////////////////////////////////
+const link = document.getElementById('result');
+
+////////////////////////////////////////////
+const wrapper = document.getElementById('calculatorButtons');
+wrapper.addEventListener('click', (event) => {
+    const isButton = event.target.nodeName === 'BUTTON';
+    if (!isButton) {
+      return;
+    }  
+    let clickedButton = event.target.id;
+    console.log(clickedButton);
+    link.textContent = clickedButton;
+  })
