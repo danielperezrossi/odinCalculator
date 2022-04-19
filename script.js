31e992e8d74db56584b6f8250ccc1e86b6a66fcf
Daniel EPR94 <danielperezrossi@gmail.com>
2022-04-18 23:05:22 -0300

    add html template

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(a,b){
+    return a+b;
+}
+function substract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+function operate(a,b){
+    let result = add(a,b);
+    return "El resultado es " + result;
+}
