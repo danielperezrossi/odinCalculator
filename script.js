21a999da489789a92c8f8669e8e77ca57a7eb7b8
Daniel EPR94 <danielperezrossi@gmail.com>
2022-05-16 23:16:00 -0300

    first working version

--- script.js
+++ script.js
@@ -1,30 +1,86 @@
-function add(a,b){
-    return a+b;
-}
-function substract(a,b){
-    return a-b;
-}
-function multiply(a,b){
-    return a*b;
-}
-function divide(a,b){
-    return a/b;
-}
-function operate(a,b){
-    let result = add(a,b);
-    return "El resultado es " + result;
-}
-////////////////////////////////////////////
-const link = document.getElementById('result');
+let operandA = '';
+let operandB = '';
+let operator = '';
+let resetScreen = false;
+
+
+const screen = document.getElementById('result');
+screen.addEventListener("DOMCharacterDataModified", (e) => {
+    let input = screen.textContent;
+    console.log("The input is:" ,input);
+    console.log("I AM ALIVE");
+})
+
 
 ////////////////////////////////////////////
 const wrapper = document.getElementById('calculatorButtons');
-wrapper.addEventListener('click', (event) => {
-    const isButton = event.target.nodeName === 'BUTTON';
-    if (!isButton) {
+wrapper.addEventListener('click', (e) => {
+    const isButton = e.target.nodeName === 'BUTTON';
+    if (!isButton){
       return;
     }  
-    let clickedButton = event.target.id;
-    console.log(clickedButton);
-    link.textContent = clickedButton;
-  })
+    let clickedButton = e.target.id;
+    screen.textContent = clickedButton;
+    /* console.log(clickedButton); */
+    newEquation(clickedButton);
+})
+
+function newEquation(input){
+    if (input ==="btnCLEAR"){
+        operandA = '';
+        operandB = '';
+        operator= '';        
+        screen.textContent = operandA;                
+    }
+    else if(input == 'btnEQUALS'){
+        screen.textContent = operate(operandA,operandB,operator);
+    }
+    else if(operator != ''){
+        operandB += parseInt(input);
+        console.log("operandB: "+operandB);
+        screen.textContent = operandB;
+    }
+    else if (!isNaN(input)){
+        operandA += parseInt(input);
+        console.log("operandA: "+operandA);
+        screen.textContent = operandA;
+    }
+    else if(input == 'btn*'){
+        operator = '*'
+        console.log("operator is: "+operator);
+    }
+    else if(input == 'btn-'){
+        operator = '-'
+        console.log("operator is: "+operator);
+    }
+    else if(input == 'btn+'){
+        operator = '+'
+        console.log("operator is: "+operator);
+    }
+    else if(input == 'btn/'){
+        operator = '/'
+        console.log("operator is: "+operator);
+    }
+}
+
+////////////////////////////////////////////
+function operate(operandA, operandB, operator) {
+    if(operator === '+') {
+        return operandA + operandB;
+    } else if(operator === '-') {
+        return operandA - operandB;
+    } else if(operator === '*') {
+        return operandA * operandB;
+    } else if(operator === '/') {
+        if(operandB === 0) {
+            return 'Not defined...yet';
+        } else {
+        return operandA / operandB;
+        }
+    }
+    
+}
+
+function clearScreen(){
+    screen.textContent = '';
+}
