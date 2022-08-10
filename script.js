09092bfcfccd06d05688fb8214f8d05cd643d4e9
Daniel EPR94 <danielperezrossi@gmail.com>
2022-08-09 23:37:29 -0300

    FIX returns statements

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let operandB = '';
 let operator = '';
 let resetScreen = false;
 
-
 const screen = document.getElementById('result');
 screen.addEventListener("DOMCharacterDataModified", (e) => {
     let input = screen.textContent;
@@ -11,37 +10,38 @@ screen.addEventListener("DOMCharacterDataModified", (e) => {
     console.log("I AM ALIVE");
 })
 
-
 ////////////////////////////////////////////
 const wrapper = document.getElementById('calculatorButtons');
 wrapper.addEventListener('click', (e) => {
     const isButton = e.target.nodeName === 'BUTTON';
     if (!isButton){
       return;
-    }  
+    }   
     let clickedButton = e.target.id;
-    screen.textContent = clickedButton;
+    /* screen.textContent = clickedButton; */
     /* console.log(clickedButton); */
     newEquation(clickedButton);
 })
 
 function newEquation(input){
-    if (input ==="btnCLEAR"){
-        operandA = '';
-        operandB = '';
-        operator= '';        
-        screen.textContent = operandA;                
+    if (input ==="btnCLEAR"){ 
+        clearScreen();       
     }
     else if(input == 'btnEQUALS'){
         screen.textContent = operate(operandA,operandB,operator);
+        console.log("The result is: " + operate(operandA,operandB,operator));
+        let result = operate(operandA,operandB,operator);
+        clearScreen();
+        operandA = result;
+        screen.textContent = operandA;
     }
     else if(operator != ''){
-        operandB += parseInt(input);
+        operandB += Number(input);
         console.log("operandB: "+operandB);
         screen.textContent = operandB;
     }
     else if (!isNaN(input)){
-        operandA += parseInt(input);
+        operandA += Number(input);
         console.log("operandA: "+operandA);
         screen.textContent = operandA;
     }
@@ -65,22 +65,50 @@ function newEquation(input){
 
 ////////////////////////////////////////////
 function operate(operandA, operandB, operator) {
+    operandA = parseInt(operandA);
+    operandB = parseInt(operandB);
+    let result = 0;
     if(operator === '+') {
-        return operandA + operandB;
+        result = operandA + operandB;
     } else if(operator === '-') {
-        return operandA - operandB;
+        result = operandA - operandB;
     } else if(operator === '*') {
-        return operandA * operandB;
+        result = operandA * operandB;
     } else if(operator === '/') {
         if(operandB === 0) {
-            return 'Not defined...yet';
+            result = 'Not defined...yet';
         } else {
-        return operandA / operandB;
+            result = operandA / operandB;
         }
     }
-    
+    console.log(result);
+    if (result % 2 === 0){
+        return result;
+    }
+    else{
+        return result.toFixed(3);
+    }
+
 }
 
 function clearScreen(){
-    screen.textContent = '';
+    operandA = '';
+    console.log(operandA);
+    operandB = '';
+    console.log(operandB);
+    operator= ''; 
+    console.log(operator);
+    screen.textContent = ' ';
+    console.log("Screen CLEARED");
 }
+
+
+/* 
+calculator pseudo algorithm
+
+screen content void
+capture first number 
+capture operator
+capture second number
+screen content result
+ */
